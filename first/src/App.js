4d1d65df49798d0abcebfee92a0e6169a3fd32ea
Zaw Ye Htet (DAT) <zawyehtet7928@gmail.com>
2023-12-21 10:55:28 +0630

    props Methods

--- first/src/App.js
+++ first/src/App.js
@@ -1,50 +1,60 @@
-import { useState, useRef } from "react";
+import { useRef, useState } from "react";
 
-function Item(prop) {
+function Item(props) {
   return (
     <li>
-      {prop.name}, {prop.price}
+      {props.name}, ${props.price}
     </li>
   );
 }
 
-export default function App() {
-  const [data, setData] = useState(
-    [
-      { id: 1, name: "Apple", price: 0.99 },
-      { id: 2, name: "Orange", price: 0.89 }
-    ]
-  );
+function AddForm(props) {
 
   const nameRef = useRef();
   const priceRef = useRef();
 
-  const add = () => {
-    const id = data.length + 1;
-    const name = nameRef.current.value;
-    const price = priceRef.current.value;
+  return (
+    <form onSubmit={e => {
+      e.preventDefault();
+      props.add(
+        nameRef.current.value,
+        priceRef.current.value
+      );
+    }}>
+      <input type="text" ref={nameRef} /> <br />
+      <input type="text" ref={priceRef} /> <br />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
 
+export default function App() {
+
+  const [data, setData] = useState([
+    { id: 1, name: "Apple", price: 0.99 },
+    { id: 2, name: "Orange", price: 0.89 },
+  ]);
+
+  const add = (name, price) => {
+    const id = data.length + 1;
     setData([
       ...data,
       { id, name, price }
     ]);
+
   }
 
   return (
     <div>
       <h1>Hello React</h1>
-
       <ul>
         {data.map(i => (
-          <Item key={i.id} name={i.name} price={i.price} />
+          <Item key={i.id}
+            name={i.name}
+            price={i.price} />
         ))}
       </ul>
-
-      <input type="text" ref={nameRef} placeholder="Name" /> <br />
-      <input type="text" ref={priceRef} placeholder="Price" /> <br />
-
-      <button onClick={add}>Add</button>
+      <AddForm add={add} />
     </div>
   );
-
 }
